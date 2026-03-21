d82c258830948de903e0e8ff08a3a25251191f9c
Mohd Anas <mohdanas08022006@gmail.com>
2026-03-21 15:16:27 +0530

    ENv cleaned

--- FrontEnd/src/Components/SendCodetoServer.jsx
+++ FrontEnd/src/Components/SendCodetoServer.jsx
@@ -1,21 +1,25 @@
 import axios from 'axios';
-const API_URL='http://localhost:8080';
+
+const API_URL = process.env.API_URL || 'http://localhost:8080';
 
 export const SendCodeToServer = async (code, input, language) => {
-    try {
-      
-        const result = await axios.post(`${API_URL}/api/runcode`, { code, input, language });
-        console.log(result.data.message);
-        return result.data;
-    }
-    catch (err) {
-      if(err.response){
-        return {error:err.response.data.message};
+  try {
+    const result = await axios.post(`${API_URL}/api/runcode`, {
+      code,
+      input,
+      language
+    });
 
-      }
-      return {
-        error:err.message,
-      }
+    console.log(result.data.message);
+    return result.data;
+
+  } catch (err) {
+    if (err.response) {
+      return { error: err.response.data.message };
     }
 
-}
+    return {
+      error: err.message,
+    };
+  }
+};

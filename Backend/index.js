a972de50c71e4debd7d8d26be3a3a647166acab7
Mohd Anas <mohdanas08022006@gmail.com>
2026-03-26 17:38:30 +0530

    health route

--- Backend/index.js
+++ Backend/index.js
@@ -1,55 +1,53 @@
-const express=require('express');
+const express = require('express');
 require('dotenv').config();
-const PORT= process.env.PORT ;
-const app=express();
-const cors=require('cors');
+const PORT = process.env.PORT;
+const app = express();
+const cors = require('cors');
 const { queue, queueEvents } = require('./Services/Producer');
 
 app.use(express.json());
 
 app.use(cors(
     {
-        origin:['http://localhost:5173','http://localhost:5174','https://online-code-editor-vert-xi.vercel.app']
+        origin: ['http://localhost:5173', 'http://localhost:5174', 'https://online-code-editor-vert-xi.vercel.app']
     }
-
-
 ))
 
 
-app.get('/health',(req,res)=>{
+app.get('/api/health', (req, res) => {
 
     return res.status(200).json({
-        message:"backend is running"
+        message: "backend is running"
     })
 })
 
 
 
-app.post('/api/runcode',async(req,res)=>{
-    const {code,input,language}=req.body;
-    const job=await queue.add('code-runner',{code,input,language});
-    const result=await job.waitUntilFinished(queueEvents);
-    
-    const {success,output,details}=result;
-    if(success===true){
+app.post('/api/runcode', async (req, res) => {
+    const { code, input, language } = req.body;
+    const job = await queue.add('code-runner', { code, input, language });
+    const result = await job.waitUntilFinished(queueEvents);
 
-      return res.status(201).json(
-        {
-        text:details,
-        message:output,
-        }
+    const { success, output, details } = result;
+    if (success === true) {
+
+        return res.status(201).json(
+            {
+                text: details,
+                message: output,
+            }
         );
 
 
     }
-    else{
+    else {
         return res.status(400).json({
-            message:details,
+            message: details,
 
         })
     }
 
-   
+
 })
 
-app.listen(PORT || 8080,()=>console.log(`server started at ${PORT}`));
+app.listen(PORT || 8080, () => console.log(`server started at ${PORT}`));
